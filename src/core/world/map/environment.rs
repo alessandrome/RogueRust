5472090d2f58236d3f9ec95b396776221524c08f
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-03-09 22:10:50 +0100

    Env - Working on Structure

--- src/core/world/map/environment.rs
+++ src/core/world/map/environment.rs
@@ -1,5 +1,16 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "env_type")]
+pub enum EnvType {
+    OpenWorld {},
+    Dungeon { max_rooms: u8 },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Environment {
-    id: u32,
+    id: String,
     name: String,
+    attributes: EnvType,
 }
