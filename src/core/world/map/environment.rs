42ee9c921e3e9862194c965bc91832e1fc31a86c
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2025-03-10 19:48:36 +0100

    Changing "env" structure to deserialize json data

--- src/core/world/map/environment.rs
+++ src/core/world/map/environment.rs
@@ -2,10 +2,24 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EnvRoomAttributes {
+    tiles: Vec<String>,
+    min_width: u8,
+    max_width: u8,
+    min_height: u8,
+    max_height: u8,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "env_type")]
 pub enum EnvType {
-    OpenWorld {},
-    Dungeon { max_rooms: u8 },
+    OpenWorld {
+        tiles: Vec<String>
+    },
+    Dungeon {
+        max_rooms: u8,
+        rooms: Vec<EnvRoomAttributes>
+    },
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
